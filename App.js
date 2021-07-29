6538c255cc5147e7028e01169c9adb1c74117445
sarahrose9 <78404100+sarahrose9@users.noreply.github.com>
2021-07-29 13:39:56 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import Header from './Header'
+import AddTask from './AddTask'
+import TaskList from './TaskList'
+import {Provider} from './Context'
+import DeleteAll from './DeleteAll'
 
 function App() {
   return (
-    <div className="App">Task Manager</div>
-  
+    <Provider>
+    <div className="app-container">
+      <Header></Header>
+      <AddTask></AddTask>
+      <DeleteAll></DeleteAll>
+      <TaskList></TaskList>
+    </div>
+    </Provider>
   );
 }
 

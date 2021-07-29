de2263c3f6881bb999c16d3c1a9841f5d1cce20b
Sarah Dias Barreto <sarah9db@yahoo.com>
2021-07-29 17:33:56 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Header from './Header'
+import AddTask from './AddTask'
+import TaskList from './TaskList'
+import {Provider} from './Context'
+import DeleteAll from './DeleteAll'
+
+function App() {
+  return (
+    <Provider>
+    <div className="app-container">
+      <Header></Header>
+      <AddTask></AddTask>
+      <DeleteAll></DeleteAll>
+      <TaskList></TaskList>
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
